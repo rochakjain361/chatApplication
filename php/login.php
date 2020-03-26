d1da9c24f236676cce7133835f669b49a6ded0d7
Ubuntu <ubuntu@ip-172-31-18-88.ec2.internal>
2020-03-26 16:57:31 +0000

    Remember Me Implemneted using Cookies

--- php/login.php
+++ php/login.php
@@ -5,17 +5,28 @@ include '../php/connect.php';
 $name = $_POST['username'];
 $pass = $_POST['password'];
 
-$query = " SELECT * FROM rochak_users WHERE name = '$name' AND passoword = '$pass' ";
+$query = " SELECT * FROM rochak_users WHERE username = '$name' AND password = '$pass' ";
 $result = $connection->query($query);
 
 $num = mysqli_num_rows($result);
-
 if($num == 1){
-    header('location:');
-    die("Successfully Logged In");
+    header('Location: ../html/chats.html');
+    if(!empty($_POST['remember'])){
+        setcookie('username',$_POST['username'],time()+ (10*365*24*60*60));
+        setcookie('passowrd',$_POST['password'],time()+ (10*365*24*60*60));
+    }
+    else{
+        if(isset($_COOKIE['username'])){
+            setcookie('username','');
+        }
+        if(isset($_COOKIE['password'])){
+            setcookie('password','');
+        }
+    }
+
 }
 else{
-    header('location:');
+    header('Location: ../php/login_new_file.php');
     die("Login Failed");
 }
- ?>
+?> 
--- php/login_new_file.php
+++ php/login_new_file.php
@@ -14,15 +14,18 @@
         <form action="../php/login.php" method="POST" onsubmit="return validation()">
             <div class = "form-group">
                 <label> Name/UserName: </label><br>
-                <input type="text" name="name" id="name" autocomplete="off">
+                <input type="text" name="username" id="name" value="<?php if(isset($_COOKIE['username'])) { echo $_COOKIE['username']; } ?>" autocomplete="off">
                 <div id="nameWarning" class="text_danger"></div>
             </div>
             <div class = "form-group"><div>
                 <label> Password: </label><br>
-                <input type="password" name="password" autocomplete="off" id="password"></div><div>
+                <input type="password" name="password" value="<?php if(isset($_COOKIE['password'])){ echo $_COOKIE['password']; } ?>" autocomplete="off" id="password"></div><div>
                 <input type="checkbox" onclick="showPassword()" style="width: 2%"><span>Show Password</span></div>
                 <div id="passwordWarning" class="text_danger"></div>
             </div>
+            <div class = "form-group">
+                 <input type="checkbox" name="remember" style="width: 2%"><span>Remember Me</span>
+       		</div>
             <div class="form-group" style="margin-left: 1em; display: flex; justify-content: center">
                  <input type="submit" style="width: 40%;height: 2em; font-size: 0.9em" autocomplete="off" id="submit">
             </div>
